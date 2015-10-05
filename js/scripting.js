7715da81dfdcdf3bcd6d411fe90c908205d68dc1
pdueker <pdueker@gmail.com>
2015-10-05 17:02:08 +0200

    js file

---
+++ js/scripting.js
@@ -0,0 +1,29 @@
+/* dynamisch json erstellen */
+$.fn.serializeObject = function()
+ {
+    var jsonObj = {};
+    var form = this.serializeArray();
+    $.each(form, function() {
+        if (jsonObj[this.name] !== undefined) {
+            if (!jsonObj[this.name].push) {
+                jsonObj[this.name] = [jsonObj[this.name]];
+            }
+            jsonObj[this.name].push(this.value || '');
+        } else {
+            jsonObj[this.name] = this.value || '';
+        }
+    });
+	console.log(jsonObj);
+	return jsonObj;
+
+ }
+
+
+/* funktion zum senden eines json über post*/
+function sendFormular(){
+    $.ajax({
+		url: 'http://localhost:8081/WIPprj/sendFormular',
+		type: 'POST',
+		data: 'jsonString='+JSON.stringify($('form').serializeObject()),
+		success: function() { alert('Daten wurden an Sachbearbeiter übermittelt'); }
+    });
